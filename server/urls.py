6363560b2b08af04a58e0177b3378231c8d039b4
Rajan Garhwal <51743092+rajangarhwal@users.noreply.github.com>
2021-04-21 20:18:18 +0530

    Update urls.py

--- server/urls.py
+++ server/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('user/<str:id>/advisor',Admin.as_view()),
     path('user/<str:id>/advisor/<int:a_id>',Adv.as_view()),
     path('user/<str:id>/advisor/booking',ADVBooking.as_view()),
+    path('user/register/', Register.as_view()),
+    path('user/login/', Login.as_view()),
+    path('admin/advisor/', Admin.as_view()),
+    path('user/<str:id>/advisor/',Admin.as_view()),
+    path('user/<str:id>/advisor/<int:a_id>/',Adv.as_view()),
+    path('user/<str:id>/advisor/booking/',ADVBooking.as_view()),
 ]
